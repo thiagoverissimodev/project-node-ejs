952bca1c26a8dae10c4acc34e460dc3a1ff82325
Thiago Verissimo <thiagovsc96@gmail.com>
2023-10-09 20:55:11 -0400

    first

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+app.use(express.static('public'));
+app.use('/css', 
+express.static(__dirname + 'public/css'))
+app.set('view engine','ejs')
+app.get("/", function(request, response){
+    response.render('pages/index')
+})
+app.get("/sobre", function(request, response){
+    response.render('pages/about')
+})
+app.get("/contato", function(request, response){
+    response.render('pages/contato')
+})
+app.listen(8080)
+console.log('Server is running');
